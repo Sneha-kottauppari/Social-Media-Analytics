e9c0dc5bacafbbf1969243fab9597f45dc899dd0
Sneha-kottauppari <kottaupparisneha@msitprogram.net>
2021-10-27 15:59:24 +0530

    pandas practice

---
+++ pandaspractice.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+d = { "name" : [ "Ellie", "Gayatri", "Rebecca", "Rishab" ],
+      "year" : [ "Junior", "Junior", "Senior", "Junior" ],
+      "major" : [ "Psychology", "CS", "CS", "MechE" ]
+    }
+df= pd.DataFrame(d)
+print(df)
+
+df["college"]=["A1","A2","A3","A4"]
+for index, row in df.iterrows():
+    print(index)
+    print(row)
+    print("Name:", row["name"])
+    print()
